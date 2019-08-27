0ebbc21a19a611ff0fb1c5cd52d61d2aeb3f3931
Andres Andrade <a.andrade09@yahoo.com>
2019-08-26 20:38:59 -0700

    Created interfaces for pet clinic services, Closes #5.

---
+++ pet-clinic-data/src/main/java/com/springframework/sfpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.springframework.sfpetclinic.services;
+
+import com.springframework.sfpetclinic.models.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
