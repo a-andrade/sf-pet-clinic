11bc112d1bf55131a70f747ec0f35f6ba5dd7f47
Andres Andrade <a.andrade09@yahoo.com>
2019-09-03 20:01:07 -0700

    Added petType service, Closes #29.

---
+++ pet-clinic-data/src/main/java/com/springframework/sfpetclinic/services/PetTypeService.java
@@ -0,0 +1,8 @@
+package com.springframework.sfpetclinic.services;
+
+import com.springframework.sfpetclinic.models.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+
+}
