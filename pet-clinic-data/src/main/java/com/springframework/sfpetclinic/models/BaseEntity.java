2291a332c31ac59350ca7dc6f4a6ace203e4009a
Andres Andrade <a.andrade09@yahoo.com>
2019-09-09 20:33:39 -0700

    Added project lombok to project, Closes #51.

--- pet-clinic-data/src/main/java/com/springframework/sfpetclinic/models/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/springframework/sfpetclinic/models/BaseEntity.java
@@ -1,23 +1,20 @@
 package com.springframework.sfpetclinic.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@Getter
+@Setter
 @MappedSuperclass // dont need to add this class to the database
 public class BaseEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
