df86f1e2518b025a297b8e150adcc2c578575dd8
Andres Andrade <a.andrade09@yahoo.com>
2019-09-25 18:51:58 -0700

    Updated owner controller and test with show owner method, added owner details form to view owner records, Closes #53.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 [![CircleCI](https://circleci.com/gh/a-andrade/sf-pet-clinic.svg?style=svg)](https://circleci.com/gh/a-andrade/sf-pet-clinic)
 
 # Spring Framework Pet Clinic
+
+This project demonstrates the use of the Spring Framework.
--- pet-clinic-web/src/main/java/com/springframework/sfpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springframework/sfpetclinic/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package com.springframework.sfpetclinic.controllers;
 import com.springframework.sfpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/owners")
@@ -30,4 +33,11 @@ public class OwnerController {
     public String errorHandler() {
         return "notimplemented";
     }*/
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
+    }
 }
