cf834bdda0a75f1539390eb20fb3616b5f7ebf3c
Andres Andrade <a.andrade09@yahoo.com>
2019-09-25 20:02:24 -0700

    Added find owner form, updated  owner controller and test with find owner method, updated owner repository / map service / spring data jpa service / service with findAllByLastNameLike() method, Closes #54.

--- pet-clinic-data/src/main/java/com/springframework/sfpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/springframework/sfpetclinic/services/OwnerService.java
@@ -2,7 +2,11 @@ package com.springframework.sfpetclinic.services;
 
 import com.springframework.sfpetclinic.models.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
